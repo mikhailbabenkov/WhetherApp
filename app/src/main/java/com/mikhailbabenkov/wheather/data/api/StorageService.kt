61b90ede8b9b8f23210b5e0ae5fa06efbaf9a02b
Mikhail Babenkov <michael.babenkov33@gmail.com>
2019-10-18 09:48:41 +1300

    -pr optimizations

--- app/src/main/java/com/mikhailbabenkov/wheather/data/api/StorageService.kt
+++ app/src/main/java/com/mikhailbabenkov/wheather/data/api/StorageService.kt
@@ -1,13 +1,7 @@
 package com.mikhailbabenkov.wheather.data.api
 
-import com.mikhailbabenkov.wheather.data.datasource.model.CityDO
-import com.mikhailbabenkov.wheather.data.datasource.model.WeatherForecastDO
-
 interface StorageService {
-    suspend fun getCities(query: String): List<CityDO>?
-    suspend fun getForecast(cityId: String): WeatherForecastDO?
-    suspend fun saveForecast(cityId: String, forecast: WeatherForecastDO)
-    suspend fun getDefaultCityId(): Int
-    suspend fun setDefaultCityId(cityId: Int)
-    suspend fun saveCities(cities: List<CityDO>)
+    suspend fun <T>read(key: String): T?
+    suspend fun <T>read(key: String, defaultValue: T): T
+    suspend fun <T>write(key: String, value: T)
 }
--- app/src/main/java/com/mikhailbabenkov/wheather/data/datasource/WeatherLocalDataSource.kt
+++ app/src/main/java/com/mikhailbabenkov/wheather/data/datasource/WeatherLocalDataSource.kt
@@ -1,31 +1,39 @@
 package com.mikhailbabenkov.wheather.data.datasource
 
+import com.mikhailbabenkov.wheather.BuildConfig
 import com.mikhailbabenkov.wheather.data.api.StorageService
 import com.mikhailbabenkov.wheather.data.datasource.model.CityDO
 import com.mikhailbabenkov.wheather.data.datasource.model.WeatherForecastDO
 
 class WeatherLocalDataSource(private val service: StorageService) {
+
     suspend fun getCities(query: String): List<CityDO>? {
-        return service.getCities(query)
+        return service.read(CITIES_KEY_PREFIX  + query)
     }
 
     suspend fun getForecast(cityId: String): WeatherForecastDO? {
-       return service.getForecast(cityId)
+       return service.read(FORECAST_KEY_PREFIX + cityId)
     }
 
     suspend fun saveForecast(cityId: String, forecast: WeatherForecastDO) {
-        return service.saveForecast(cityId, forecast)
+        return service.write(FORECAST_KEY_PREFIX + cityId, forecast)
     }
 
     suspend fun saveCities(cities: List<CityDO>) {
-        service.saveCities(cities)
+        service.write(CITIES_KEY_PREFIX, cities)
     }
 
     suspend fun getDefaultCityId(): Int {
-        return service.getDefaultCityId()
+        return service.read(DEFAULT_CITY_KEY, BuildConfig.DEFAULT_CITY_ID)
     }
 
-    suspend fun setDefaultCityId(city: Int) {
-        service.setDefaultCityId(city)
+    suspend fun setDefaultCityId(cityId: Int) {
+        service.write(DEFAULT_CITY_KEY, cityId)
+    }
+
+    companion object{
+        private const val CITIES_KEY_PREFIX = "cities-key-prefix_"
+        private const val FORECAST_KEY_PREFIX = "forecast-key-prefix_"
+        private const val DEFAULT_CITY_KEY = "default-city"
     }
 }
